f98988ebb3c8a7fff891b095f9f6ae5aa96d042b
Lejla1502 <lejlasphotmail.com>
2021-12-16 08:55:37 +0100

    Adding wildcar rule for routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,10 @@ const appRoutes:Routes=[
   { path:'servers', component: ServersComponent, children:[
     { path:':id/edit', component: EditServerComponent},
     { path:':id', component: ServerComponent}
-  ]}
+  ]},
+
+  {path: 'not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/not-found'}
 
 ];
   
