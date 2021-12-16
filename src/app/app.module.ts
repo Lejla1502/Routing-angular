3e0b2158bc9aa23a6bd33e97ccd7590c43415d13
Lejla1502 <lejlasphotmail.com>
2021-12-16 10:02:42 +0100

    Passing route config in app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,24 +11,10 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ApproutingModule } from './app-routing.module';
 
-const appRoutes:Routes=[
-  {path:'', component: HomeComponent},
-  { path:'users', component: UsersComponent, children:[
-    { path:':id/:name', component: UserComponent}
-  ]},  
-  
-  { path:'servers', component: ServersComponent, children:[
-    { path:':id/edit', component: EditServerComponent},
-    { path:':id', component: ServerComponent}
-  ]},
 
-  {path: 'not-found', component: PageNotFoundComponent},
-  {path: '**', redirectTo: '/not-found'}
-
-];
   
 
 
@@ -46,7 +32,7 @@ const appRoutes:Routes=[
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    ApproutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
